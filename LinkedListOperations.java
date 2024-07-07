974371835c81570c059744f132f5ef5efab48fd2
YashrajDodia <140829137+YashrajDodia@users.noreply.github.com>
2024-07-07 23:13:07 +0530

    Update LinkedListOperations.java

--- LinkedListOperations.java
+++ LinkedListOperations.java
@@ -1,8 +1,6 @@
-
-
 class Node{
     int data;
-    Node link = null;
+    Node link;
     public Node(int data){
         this.data = data;
     }
@@ -11,32 +9,102 @@ class Node{
 class LinkedList{
     Node first;
     public void insertAtFirst(int data){
-        Node n1 = new Node(data);
+        Node n = new Node(data);
         if(first==null){
-            first = n1;
+            first = n;
         }else{
-            n1.link = first;
-            first = n1;
+            n.link = first;
+            first = n;
         }
     }
 
+    public void insertAtLast(int data){
+        Node n = new Node(data);
+        Node save = first;
+        if(first==null){
+            first = n;
+        }else{
+            while(save.link!=null){
+                save = save.link;
+            }
+            save.link = n;
+        }
+    }
+
+    public void deleteNode(int data){
+        if(first==null){
+            System.out.println("Linked List is empty");
+        }else{
+            Node save = first;
+            Node temp = save;
+            if(data==first.data){
+                first = first.link;
+                return;
+            }
+            while((save.link!=null)&&(save.data!=data)){
+                temp = save;
+                save = save.link;
+            }
+            if((save.link==null)&&(save.data!=data)){
+                System.out.println("Node not found !");
+            }else{
+                temp.link = save.link;
+            }    
+        }
+    }
+
+    public void countNodes(){
+        int count = 0;
+        if(first==null){
+            System.out.println("Linked List is empty");
+            return;
+        }
+        Node temp = first;
+        while(temp!=null){
+            count++;
+            temp = temp.link;
+        }
+        System.out.println("Total no. of nodes in linked list are : " + count);
+    }
+    //insert at first , insert at last , delete node , copy linked list , count nodes 
+    public LinkedList copyLinkedList(Node first,LinkedList l){
+        LinkedList l1 = new LinkedList();
+        Node save = first;
+        while(save!=null){
+            l1.insertAtLast(save.data);
+            save = save.link;
+        }
+        return l1;
+    }
+
     public void display(){
+        if(first==null){
+            System.out.println("Linked List is empty");
+        }
         Node temp = first;
         while(temp!=null){
             System.out.print(temp.data + " ");
             temp = temp.link;
         }
+        System.out.println();
     }
 }
+
 public class LinkedListOperations {
-    public static void main(String[] args) {
-       LinkedList l1 = new LinkedList();
-       l1.insertAtFirst(5);
-       l1.insertAtFirst(6);
-       l1.insertAtFirst(7);
-       l1.insertAtFirst(8);
-       l1.insertAtFirst(9);
-       l1.insertAtFirst(10);
-       l1.display();
+    public static void main(String[] args){
+        LinkedList l1 = new LinkedList();
+        l1.insertAtLast(5);
+        l1.insertAtLast(10);
+        l1.insertAtLast(15);
+        l1.insertAtLast(20);
+        l1.insertAtLast(25);
+        l1.insertAtLast(30);
+        l1.insertAtLast(35);
+        LinkedList l2 = l1.copyLinkedList(l1.first, l1);
+        l2.deleteNode(25);
+        l2.display();
+        l2.countNodes();
+        l1.display();
+        l1.countNodes();
     }
 }
