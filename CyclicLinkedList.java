26565f775d4a8435ab3680c6333686cb3ae2dfac
YashrajDodia <140829137+YashrajDodia@users.noreply.github.com>
2024-07-25 00:05:47 +0530

    Update CyclicLinkedList.java

--- CyclicLinkedList.java
+++ CyclicLinkedList.java
@@ -16,12 +16,59 @@ class LinkedList{
             last = n;
             first.link = last;
         }else{
-            n.link = first;
+            n.link = last.link;
             last.link = n;
             first = n;
         }
     }
-    //5,10,15,20,25
+
+    public void insertAtLast(int data){
+        Node n = new Node(data);
+        if(first==null){
+            first = n;
+            last = n;
+            first.link = last;
+        }else{
+            n.link=last.link;
+            last.link = n;
+            last = n;
+        }
+    }
+    public void deleteElement(int data){
+        Node save = first;
+        Node temp = save;
+        if(first.data==data){
+            last.link=first.link;
+            first = first.link;
+            return;
+        }
+        else{
+            while (save.link!=first) {
+                if(save.data==data){
+                    temp.link=save.link;
+                    return;
+                }else{
+                    temp = save;
+                    save = save.link;
+                }
+            }
+            if(save.data!=data){
+                System.out.println("No node found !!");
+            }else{
+                temp.link = first;
+                last = temp;
+            }
+        }
+    }
+
+    public void insertInOrder(int data){
+        if(data<=first.data){
+            insertAtFirst(data);
+        }else{
+            
+        }
+    }
+
     public void display(){
        if(first==null){
         System.out.println("Circular linked list is empty !");
@@ -31,7 +78,9 @@ class LinkedList{
         System.out.print(save.data + " ");
         save = save.link;
        }
-       System.out.print(save.data);
+       System.out.println(save.data);
+       System.out.println("First node is : " + first.data);
+       System.out.println("Last node is : " + last.data);
        }
     }
 }
@@ -45,6 +94,10 @@ public class CyclicLinkedList {
         l1.insertAtFirst(15);
         l1.insertAtFirst(20);
         l1.insertAtFirst(25);
+        l1.insertAtLast(30); 
+        l1.insertAtLast(35); 
+        l1.insertAtLast(40); 
+        l1.deleteElement(10);       
         l1.display();
     }
 }
